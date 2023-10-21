45b0d9333393c1ca118ae0ed38b6c3c6a8f10f98
Aytaç Kayadelen <aytackayadelen@gmail.com>
2023-10-21 16:30:12 +0200

    update calculations for Astronomical Twilight

--- src/Artemis.Plugins.CelestialData/CelestialDataModel.cs
+++ src/Artemis.Plugins.CelestialData/CelestialDataModel.cs
@@ -15,7 +15,8 @@ public class CelestialDataModel : DataModel
         Description = "Value in 0-100. 100 means sun is directly above you")]
     public double SolarNoonPercentage
     {
-        get {
+        get
+        {
             var dateTime = DateTime.Now;
 
             if (dateTime.Hour != _currentHour)
@@ -30,15 +31,27 @@ public class CelestialDataModel : DataModel
 
     private void GenerateHourData(DateTime date)
     {
+
         _currentHourValues = new Dictionary<int, double>();
-        
+
         date = date.AddMinutes(-date.Minute);
         for (var i = 0; i < 60; i++)
         {
-            var coordinate = new Coordinate(CelestialModule.Lat, CelestialModule.Lon, date);
-            var noonPercentage = coordinate.CelestialInfo.SunAltitude * 100d / 90;
+            var coordinate = new Coordinate(52.4928, 13.4039, date);
+            var noonPercentage = BrightnessFunction(coordinate.CelestialInfo.SunAltitude);
             _currentHourValues[i] = Math.Clamp(noonPercentage, 0d, 100d);
             date = date.AddMinutes(1);
         }
+    }
+
+    private static double BrightnessFunction(double x)
+    {
+        const double noonAngle = 90;        //x2
+        const double darknessAngle = -18;   //x1
+        //linear plotting   (y2 - y1)(x2 - x1)
+        const double m = (100 - 0) / (noonAngle - darknessAngle);
+        
+        // y = m(x - x1) + y1
+        return m * (x - darknessAngle) + 0;
     }
 }
--- src/Artemis.Plugins.CelestialData/IpApi/IpData.cs
+++ src/Artemis.Plugins.CelestialData/IpApi/IpData.cs
@@ -8,10 +8,10 @@ public class IpData
     public double Lon { get; }
 
     public string Status { get; }
-    public string Message { get; }
+    public string? Message { get; }
 
     [JsonConstructor]
-    public IpData(double lat, double lon, string status, string message)
+    public IpData(double lat, double lon, string status, string? message)
     {
         Lat = lat;
         Lon = lon;
