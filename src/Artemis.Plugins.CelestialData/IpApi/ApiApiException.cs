5aa0111d19974f5597df4ad5e1329591b8ec2583
Aytaç Kayadelen <aytackayadelen@gmail.com>
2024-03-20 23:55:43 +0100

    fix json namings for System.Text.Json migration

---
+++ src/Artemis.Plugins.CelestialData/IpApi/ApiApiException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Artemis.Core;
+
+namespace Artemis.Plugins.CelestialData.IpApi;
+
+public class ApiApiException : ArtemisPluginException
+{
+    public IpData? Response { get; }
+
+    public ApiApiException(string message, IpData? response) : base(message)
+    {
+        Response = response;
+    }
+
+    public ApiApiException(string message, IpData? response, Exception inner) : base(message, inner)
+    {
+    }
+}
--- src/Artemis.Plugins.CelestialData/IpApi/IpApi.cs
+++ src/Artemis.Plugins.CelestialData/IpApi/IpApi.cs
@@ -1,24 +1,30 @@
 ﻿using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
-using Artemis.Core;
 
 namespace Artemis.Plugins.CelestialData.IpApi;
 
 public static class IpApiClient
 {
+    private static readonly JsonSerializerOptions JsonOptions = new()
+    {
+        TypeInfoResolverChain = { IpApiSourceGenerationContext.Default },
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    };
+    
     public static async Task<IpData?> GetIpData()
     {
         using HttpClient client = new();
-        var response = await client.GetFromJsonAsync("http://ip-api.com/json?fields=status,message,lat,lon", IpApiSourceGenerationContext.Default.IpData);
+        var response = await client.GetFromJsonAsync<IpData>("http://ip-api.com/json?fields=status,message,lat,lon", JsonOptions);
 
         if (response == null)
         {
-            throw new ArtemisPluginException("IpApi returned empty response");
+            throw new ApiApiException("IpApi returned empty response", response);
         }
         if (response.Status != "success")
         {
-            throw new ArtemisPluginException("IpApi returned error: " + response.Message);
+            throw new ApiApiException("IpApi returned error: " + response.Message, response);
         }
         
         return response;
